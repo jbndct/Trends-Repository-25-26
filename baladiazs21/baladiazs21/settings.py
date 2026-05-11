f37a21dae3f6415e50679060d6dde3040a91d2a1
John Benedict Baladia <johnbenedict.baladia@gmail.com>
2026-05-12 00:35:22 +0800

    Coding Practice 3

--- baladiazs21/baladiazs21/settings.py
+++ baladiazs21/baladiazs21/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nu79*sfu8rl8=%7rr=el4pzk%6jdy5=1&ieb-*0b*f_lz%flt0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members'
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +75,11 @@ WSGI_APPLICATION = 'baladiazs21.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME' : 'test1',
+    'USER' : 'Member',
+    'PASSWORD' : 'member',
+}
 }
 
 
